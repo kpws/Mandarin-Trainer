e21e510dbba4d057a72ab30dd9616e814e78f33f
Petter Säterskog <petter.saterskog@gmail.com>
2011-11-23 01:33:48 +0800

    some mods

--- mandarinTrainer.py
+++ mandarinTrainer.py
@@ -10,41 +10,34 @@ with open(os.path.join(os.path.dirname(__file__), 'mixChars'),'r') as mixCharFil
 toChinese = lambda english: chars[[c[2] for c in chars].index(english)][0]
 
 mixCharRatio=0.1
-
-with open(os.path.expanduser('~/.mandarinTrainerHist'),'ra+') as histFile:
+red='\033[31m'
+yellow='\033[33m'
+green='\033[32m'
+blue='\033[34m'
+purple='\033[36m'
+standard='\033[0;m'
+with open(os.path.expanduser('~/.mandarinTrainerHist'),'a+') as histFile:
 	hist=[l for l in histFile]
 	print hist
-	done=0
-	correct=0
 	while True:
+		if random.random()<mixCharRatio:
+		    char=mixChars[random.randint(0,len(chars)-1)]
+		    correct=''
+		else:
+		    i=random.randint(0,len(chars)-1)
+		    char=chars[i][0]
+		    correct=chars[i][2]
+		print(yellow+char+':')
 		try:
-			if random.random()<mixCharRatio:
-				print('\033[33m'+mixChars[random.randint(0,len(chars)-1)]+':'+'\033[0;m')
-				if raw_input('')=='':
-					print('\033[32m'+toChinese('right')+'\033[0;m')
-					correct+=1
-					histFile.write('dd\t'+str(time.time())+'\n')
-				else:
-					print('\033[31m'+toChinese('not')+toChinese('right')+','+
-						toChinese('it')+toChinese('is')+':\033[0;m')
-					histFile.write('ddf\n')
+			if raw_input('')==correct:
+				print(green+toChinese('right'))
+				histFile.write('dd\t'+str(time.time())+'\n')
 			else:
-				i=random.randint(0,len(chars)-1)
-				print('\033[33m'+chars[i][0]+':'+'\033[0;m')
-				if raw_input('').lower()==chars[i][2]:
-					print('\033[32m'+toChinese('right')+'\033[0;m')
-					correct+=1
-					histFile.write('asdf\n')
-				else:
-					print('\033[31m'+toChinese('not')+toChinese('right')+','+
-						toChinese('it')+toChinese('is')+': '+chars[i][2]+'\033[0;m')
-					histFile.write('fefe\n')
-				print('\033[34m'+toChinese('pinyin')+': '+chars[i][1]+'\033[0;m')
-			done+=1
+				print(red+toChinese('not')+toChinese('right')+','+
+					toChinese('it')+toChinese('is')+': '+correct)
+				histFile.write('ddf\n')
+				if correct:
+					print(blue+toChinese('pinyin')+': '+chars[i][1])
 		except KeyboardInterrupt:
-			if done:
-				print('\n'+str(correct*100//done)+'% '+toChinese('right'))
-				if correct*100>=done*95:
-					print(toChinese('very')+toChinese('good'))
-			print(toChinese('goodbye'))
+			print(purple+'\n'+toChinese('goodbye')+standard)
 			break
