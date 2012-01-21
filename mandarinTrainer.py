5e95594d5bfcd2aeaa9468432c8137af25ebc7d7
Petter Säterskog <petter@localhost.localdomain>
2012-01-21 18:27:11 +0100

    Added licensing iformation

--- mandarinTrainer.py
+++ mandarinTrainer.py
@@ -1,3 +1,18 @@
+''' Copyright 2011 Petter Saeterskog
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.'''
+
 import time, os; from random import random, randrange
 with open(os.path.join(os.path.dirname(__file__), 'chars'),'r') as charFile:
     chars=[l.replace('\t','').rstrip('\n').rsplit(',') for l in charFile if l[0]!='#']
